0221f6cb963df95bb8f9c815fe015630a9c726a6
Aries Dmello <dmelloaries@gmail.com>
2024-09-17 01:18:06 +0530

    fixed cors issue for frontend

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const cors = require('cors'); // Import cors
+const cors = require('cors'); 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
-app.use(cors()); 
 
-// Enable CORS for the frontend running on http://localhost:5173
+
 app.use(cors({
-  origin: 'https://quadbassignment.vercel.app/'
+  origin: 'https://quadbassignment.vercel.app'
 }));
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("Server is live");
-  });
+});
   
 // Route to get all tickers
 app.get('/tickers', async (req, res) => {
